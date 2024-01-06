bb09891e45c250fdec92f751af9dea7cfce892a6
joaovian06 <joaovitor_vian@hotmail.com>
2024-01-06 13:22:36 -0300

    json associations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,7 +5,14 @@ class Contact < ApplicationRecord
         "joao vian"
     end
 
+    def kind_description
+        self.kind.description
+    end
+
     def as_json(options={})
-        super(methods: :author, root: true)
+        super(
+            methods: [:kind_description, :author],
+            root: true
+        )
     end
 end
