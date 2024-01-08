05bf1958c2a6fe98be2e9386e4c4502a847374dd
joaovian06 <joaovitor_vian@hotmail.com>
2024-01-08 09:53:06 -0300

    cleaning serialized json and add foreman gem to project

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,14 +1,14 @@
 class ContactSerializer < ActiveModel::Serializer
   # Associations
   belongs_to :kind do
-    link(:kind) { kind_url(object.kind.id) }
+    link(:related) { kind_url(object.kind.id) }
   end
 
   has_many :phones
   has_one :address
 
   # Hateaos links
-  link(:self) { contact_url(object.id) }
+  # link(:self) { contact_url(object.id) }
 
   attributes :id, :name, :email, :birthdate#, :author
 
