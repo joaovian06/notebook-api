97c7abc5eb106132bed8cdbce48e183b585839ed
joaovian06 <joaovitor_vian@hotmail.com>
2024-01-08 11:19:26 -0300

    compound documents for kinds resource

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,7 +1,7 @@
 class ContactSerializer < ActiveModel::Serializer
   # Associations
   belongs_to :kind do
-    link(:related) { kind_url(object.kind.id) }
+    link(:related) { contact_kind_url(object.id) }
   end
 
   has_many :phones
