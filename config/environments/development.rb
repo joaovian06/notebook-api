8dcd27e5e8eb17499d283d2ba627f4a6baa6cd54
joaovian06 <joaovitor_vian@hotmail.com>
2024-01-10 14:26:08 -0300

    config for host versioning

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
     host: 'localhost',
     port: 3000
   }
+
+  config.hosts << "meusite.local:3000"
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
